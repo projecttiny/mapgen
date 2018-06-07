ff7ce630bc67c8b363bd0a35f4d28550f77855ca
ebogo1 <ebogo@seas.upenn.edu>
2018-06-06 21:48:16 -0400

    voxel placement

--- js/src/WorldGrid.js
+++ js/src/WorldGrid.js
@@ -1,9 +1,23 @@
 class WorldGrid {
-    constructor() { }
-    
-    getColor() {
-        return 0.5;
+    constructor(x, y, z) { 
+        this.x = x;
+        this.y = y;
+        this.z = z;
+
+        this.grid = new Array();        
+        this.initGrid();        
+    }
+
+    initGrid() {
+        for(var x = 0; x < this.x; ++x) {
+            this.grid[x] = new Array();
+            for(var y = 0; y < this.y; ++y) {
+                this.grid[x][y] = new Array();
+                for(var z = 0; z < this.z; ++z)
+                {
+                    this.grid[x][y][z] = Math.round(Math.random());                    
+                }
+            }
+        }      
     }
 }
-
-
