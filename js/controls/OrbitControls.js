ac5fbdaaaaf3e0f58421bf03c3a18105f9491e61
ebogo1 <ebogo@seas.upenn.edu>
2018-06-08 22:55:26 -0400

    minor adjustments

--- js/controls/OrbitControls.js
+++ js/controls/OrbitControls.js
@@ -50,7 +50,7 @@ THREE.OrbitControls = function ( object, domElement ) {
 
 	// This option actually enables dollying in and out; left as "zoom" for backwards compatibility.
 	// Set to false to disable zooming
-	this.enableZoom = true;
+	this.enableZoom = false;
 	this.zoomSpeed = 1.0;
 
 	// Set to false to disable rotating
